a71bd2ecf726d307892e771a01ca99b0fedf3e4c
youssef-hassane <youssef_hassane@icloud.com>
2024-01-18 07:58:10 +0000

    Adding the pint operation

--- pint_operation.c
+++ pint_operation.c
@@ -1,6 +1,5 @@
 #include "monty.h"
 
-
 /**
  * pint_operation - Handles the "pint" operation.
  * Return: None.
@@ -8,18 +7,21 @@
  * Prototype: void pint_operation(stack_t **head, unsigned int counter);
  * --------------------------
  * @head: Pointer to the top of the stack.
- * @counter: Current line number in the script file.
+ * @theNumberOfTheLine: Current line number in the script file.
  * --------------------------
  * Description: Prints the value at the top of the stack.
  * If the stack is empty, prints an error message and exits.
  * --------------------------
  * By Youssef Hassane AKA Almasy
-*/
-void pint_operation(stack_t **head, unsigned int counter)
+ */
+void pint_operation(stack_t **head, unsigned int theNumberOfTheLine)
 {
+	char error_message[100];
+
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		sprintf(error_message, "L%u: can't pint, stack empty\n", theNumberOfTheLine);
+		write(STDERR_FILENO, error_message, strlen(error_message));
 		fclose(montyExecutionContext.theFile);
 		free(montyExecutionContext.theBuffer);
 		release_stack(*head);
